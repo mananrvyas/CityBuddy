ce49075ee8311285adbe42220461b010c7b09690
Arleen Monteiro <111902870+ArleenMonteiro@users.noreply.github.com>
2023-01-28 20:28:52 -0500

    Update script.js
    adding current location detector with marker

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function successLocation(position) {
   setupMap([position.coords.longitude, position.coords.latitude])
 }
 
+
 function errorLocation() {
   setupMap([-2.24, 53.48])
 }
@@ -22,6 +23,69 @@ function setupMap(center) {
     zoom: 15
   })
 
+  // const geojson = {
+  //   type: 'FeatureCollection',
+  //   features: [
+  //     {
+  //       type: 'Feature',
+  //       geometry: {
+  //         type: 'Point',
+  //         coordinates: [-77.032, 38.913]
+  //       },
+  //       properties: {
+  //         title: 'Mapbox',
+  //         description: 'Washington, D.C.'
+  //       }
+  //     },
+  //     {
+  //       type: 'Feature',
+  //       geometry: {
+  //         type: 'Point',
+  //         coordinates: [-122.414, 37.776]
+  //       },
+  //       properties: {
+  //         title: 'Mapbox',
+  //         description: 'San Francisco, California'
+  //       }
+  //     }
+  //   ]
+  // };
+
+//   // add markers to map
+// for (const feature of geojson.features) {
+//   // create a HTML element for each feature
+//   const el = document.createElement('div');
+//   el.className = 'marker';
+
+  // make a marker for each feature and add to the map
+//   new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
+// }
+var geolocate = new mapboxgl.GeolocateControl({
+  positionOptions: {
+    enableHighAccuracy: true
+  },
+  trackUserLocation: true
+});
+// Add the control to the map.
+map.addControl(geolocate);
+map.on('load', function() {
+  geolocate.trigger();
+});
+geolocate.on('geolocate', locateUser);
+
+function locateUser(e) {
+  console.log('A geolocate event has occurred.');
+  console.log("lng:" + e.coords.longitude + ", lat:" + e.coords.latitude)
+    geolocate.off('geolocate', null);
+}
+
+function locateUser(e) {
+  console.log('A geolocate event has occurred.');
+  console.log("lng:" + e.coords.longitude + ", lat:" + e.coords.latitude)
+    geolocate.off('geolocate', null);
+}
+
+
   const nav = new mapboxgl.NavigationControl()
   map.addControl(nav)
 
@@ -31,3 +95,4 @@ function setupMap(center) {
 
   map.addControl(directions, "top-left")
 }
+
